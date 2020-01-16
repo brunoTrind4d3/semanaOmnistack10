cee2274476e9e3800d224846b03250c095f412b0
Bruno Trindade <bruno.eng.ufg@gmail.com>
2020-01-15 23:41:47 -0300

    Cria estilos e componentes react

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routes");
 const app = express();
 
@@ -10,7 +11,8 @@ mongoose.connect(
     useUnifiedTopology: true
   }
 );
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 app.listen(3333);
--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,187 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
-  return <div id="app"></div>;
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      err => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    );
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post(
+      '/devs',
+      github_username,
+      techs,
+      latitude,
+      longitude
+    );
+    console.log(response.data);
+    setGithubUsername('');
+    setTechs('');
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do github</label>
+            <input
+              name="github_username"
+              id="github_username"
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              name="techs"
+              id="techs"
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)}
+            ></input>
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              ></input>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              ></input>
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/49998605?s=400&u=8e5960f291e93b224319f3e35208624fe9ad9ff6&v=4"
+                alt="brunoTrind4d3"
+              />
+              <div className="user-info">
+                <strong>Bruno Trindade</strong>
+                <span>ReactJs, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              Graduando em Análise e Desenvolvimento de sistemas -SENAI FATESG.
+              Java Web Developer. Flutter Mobile Developer.
+            </p>
+            <a href="http://github.com/brunoTrind4d3">
+              Acessar perfil no Github
+            </a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/49998605?s=400&u=8e5960f291e93b224319f3e35208624fe9ad9ff6&v=4"
+                alt="brunoTrind4d3"
+              />
+              <div className="user-info">
+                <strong>Bruno Trindade</strong>
+                <span>ReactJs, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              Graduando em Análise e Desenvolvimento de sistemas -SENAI FATESG.
+              Java Web Developer. Flutter Mobile Developer.
+            </p>
+            <a href="http://github.com/brunoTrind4d3">
+              Acessar perfil no Github
+            </a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/49998605?s=400&u=8e5960f291e93b224319f3e35208624fe9ad9ff6&v=4"
+                alt="brunoTrind4d3"
+              />
+              <div className="user-info">
+                <strong>Bruno Trindade</strong>
+                <span>ReactJs, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              Graduando em Análise e Desenvolvimento de sistemas -SENAI FATESG.
+              Java Web Developer. Flutter Mobile Developer.
+            </p>
+            <a href="http://github.com/brunoTrind4d3">
+              Acessar perfil no Github
+            </a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars0.githubusercontent.com/u/49998605?s=400&u=8e5960f291e93b224319f3e35208624fe9ad9ff6&v=4"
+                alt="brunoTrind4d3"
+              />
+              <div className="user-info">
+                <strong>Bruno Trindade</strong>
+                <span>ReactJs, React Native, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              Graduando em Análise e Desenvolvimento de sistemas -SENAI FATESG.
+              Java Web Developer. Flutter Mobile Developer.
+            </p>
+            <a href="http://github.com/brunoTrind4d3">
+              Acessar perfil no Github
+            </a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
